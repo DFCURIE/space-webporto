e0eb605c6872382ad2e1e505136caa271d5c4b62
dfcurie <difamuhammad28@gmail.com>
2024-03-19 19:24:25 +0700

    updated better.tsx

--- components/main/Better.tsx
+++ components/main/Better.tsx
@@ -8,28 +8,28 @@ const Better = () => {
     <section id="about" style={{ padding: '20px' }}>
       <h1 className="title" style={{ color: 'white', fontSize: '2rem', textAlign: 'center', marginBottom: '50px' }}>Get To Know More About Me</h1>
       <div className="section-container" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <div className="section__pic-container" style={{ marginBottom: '50px', position: 'relative', padding: '15px', borderRadius: '50%', display: 'flex', justifyContent: 'center' }}>
-          <div style={{ border: '0.5px solid white', borderRadius: '8px', overflow: 'hidden', margin: '0 10px', maxWidth: '50%', width: '100%' }}>
-            <Image src="/PPDF.jpg" alt="Profile picture" className="about-pic" layout="responsive" width={300} height={300} />
+        <div className="section__pic-container" style={{ marginBottom: '50px', position: 'relative', padding: '15px', borderRadius: '8px', display: 'flex', justifyContent: 'center' }}>
+          <div style={{ border: '0.2px solid white', borderRadius: '8px', overflow: 'hidden', margin: '0 10px' }}>
+            <Image src="/PPDF.jpg" alt="Profile picture" className="about-pic" layout="fixed" width={150} height={150} quality={80} loading="lazy" />
           </div>
-          <div style={{ border: '0.5px solid white', borderRadius: '8px', overflow: 'hidden', margin: '0 10px', maxWidth: '50%', width: '100%' }}>
-            <Image src="/aboutme.jpg" alt="About Me" className="about-pic" layout="responsive" width={300} height={300} />
+          <div style={{ border: '0.2px solid white', borderRadius: '8px', overflow: 'hidden', margin: '0 10px' }}>
+            <Image src="/aboutme.jpg" alt="About Me" className="about-pic" layout="fixed" width={150} height={150} quality={80} loading="lazy" />
           </div>
         </div>
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <div style={{ borderRadius: '2rem', border: '0.1rem solid rgb(231, 229, 229)', padding: '20px 40px', marginBottom: '20px', width: '100%' }}>
-            <div style={{ marginBottom: '10px', textAlign: 'center' }}>
-              <Image src="/experience.png" alt="Experience Logo" width={30} height={30} />
+          <div style={{ borderRadius: '1rem', border: '0.05rem solid rgb(231, 229, 229)', padding: '10px 20px', marginBottom: '20px', width: '100%' }}>
+            <div style={{ marginBottom: '5px', textAlign: 'center' }}>
+              <Image src="/experience.png" alt="Experience Logo" width={25} height={25} />
             </div>
-            <p style={{ color: 'white', margin: '0', fontSize: '1rem', fontWeight: 'bold', textAlign: 'center' }}>Experience</p>
-            <p style={{ color: 'white', margin: '0', fontSize: '0.9rem', textAlign: 'center' }}>1 Year Junior Full Stack Engineer</p>
+            <p style={{ color: 'white', margin: '0', fontSize: '0.9rem', fontWeight: 'bold', textAlign: 'center' }}>Experience</p>
+            <p style={{ color: 'white', margin: '0', fontSize: '0.8rem', textAlign: 'center' }}>1 Year Junior Full Stack Engineer</p>
           </div>
-          <div style={{ borderRadius: '2rem', border: '0.1rem solid rgb(231, 229, 229)', padding: '20px 40px', marginBottom: '20px', width: '100%' }}>
-            <div style={{ marginBottom: '10px', textAlign: 'center' }}>
-              <Image src="/education.png" alt="Education Logo" width={30} height={30} />
+          <div style={{ borderRadius: '1rem', border: '0.05rem solid rgb(231, 229, 229)', padding: '10px 20px', marginBottom: '20px', width: '100%' }}>
+            <div style={{ marginBottom: '5px', textAlign: 'center' }}>
+              <Image src="/education.png" alt="Education Logo" width={25} height={25} />
             </div>
-            <p style={{ color: 'white', margin: '0', fontSize: '1rem', fontWeight: 'bold', textAlign: 'center' }}>Education</p>
-            <p style={{ color: 'white', margin: '0', fontSize: '0.9rem', textAlign: 'center' }}>Bachelors Of Computer Engineering</p>
+            <p style={{ color: 'white', margin: '0', fontSize: '0.9rem', fontWeight: 'bold', textAlign: 'center' }}>Education</p>
+            <p style={{ color: 'white', margin: '0', fontSize: '0.8rem', textAlign: 'center' }}>Bachelors Of Computer Engineering</p>
           </div>
         </div>
         <div className="about-details-container" style={{ width: '100%' }}>
--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -2,8 +2,6 @@ import {
     Backend_skill,
     Frontend_skill,
     Full_stack,
-    Other_skill,
-    Skill_data,
   } from "@/constants";
   import React from "react";
   import SkillDataProvider from "../sub/SkillDataProvider";
@@ -42,17 +40,6 @@ import {
         </div>
         <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
           {Full_stack.map((image, index) => (
-            <SkillDataProvider
-              key={index}
-              src={image.Image}
-              width={image.width}
-              height={image.height}
-              index={index}
-            />
-          ))}
-        </div>
-        <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-          {Other_skill.map((image, index) => (
             <SkillDataProvider
               key={index}
               src={image.Image}
